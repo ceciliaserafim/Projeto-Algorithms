7f0512716a059e6409f84be846e4d60ef2fdab43
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-27 13:42:49 -0300

    Requisito 1: Quantidade de alunos presentes na entrada específica

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    students = 0
+    for star_period, finish_period in permanence_period:
+        if isinstance(star_period, int) or isinstance(finish_period, int):
+            return None
+# Retorne a quantidade de estudantes presentes para uma entrada específica;
+    if star_period <= target_time <= finish_period:
+        students += 1
+    # raise NotImplementedError
