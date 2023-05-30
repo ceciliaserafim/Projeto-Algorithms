cf5cd548c2d5a34640560a3d04a5616f474394b3
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-30 14:30:40 -0300

    Adiciona requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word, first_letter, last_letter):
+    if word == '':
+        return False
+    if first_letter >= last_letter:
+        return True
+    if word[first_letter] == word[last_letter]:
+        return is_palindrome_recursive(word, first_letter + 1, last_letter - 1)
+    else:
+        return False
