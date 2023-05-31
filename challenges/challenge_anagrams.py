14003cbd4fbbdcd6f4d8b1eec44d88a14f884016
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-31 10:38:10 -0300

    Adiciona testes, requisito 2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,15 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # converte as strings em lista de letras, as embaralham e case insensitive
+    letters_first_string = sorted(list(first_string).lower())
+    letters_second_string = sorted(list(second_string).lower())
+    # as listas de letras voltam a ser palavras
+    first_string = ''.join(letters_first_string)
+    second_string = ''.join(letters_second_string)
+
+    if first_string or second_string == '':
+        return False
+
+    if first_string == second_string:
+        return True
+    else:
+        return False
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(message="message", key=908) == "egassem"
+    assert encrypt_message(message="message2", key=2) == "2egass_em"
+    assert encrypt_message(message="message3", key=3) == "sem_3egas"
+
+    # tipo incorreto para key
+    with pytest.raises(TypeError):
+        encrypt_message(message="incorreto para key", key="")
+    # tipo incorreto para message
+    with pytest.raises(TypeError):
+        encrypt_message(message=656, key=3)
